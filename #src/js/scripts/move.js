aaa01379098c969906d33ee75b02e0c7b86323ca
Stas Colocolov <colocolov.stas@gmail.com>
2022-09-06 01:50:39 +0300

    adaptiv up to 768px

---
+++ #src/js/scripts/move.js
@@ -0,0 +1,21 @@
+const scrWidth = window.screen.width;
+const priceBtn = document.querySelector(".prices__btn");
+const pricesContainer = document.querySelector(".prices__container");
+const hygieneHead = document.querySelector(".hygiene__head");
+const hygieneImg = document.querySelector(".hygiene__img");
+
+console.log(pricesContainer);
+
+const moveEl = function () {
+  // move button on PRICE
+  priceBtn.remove();
+  pricesContainer.insertAdjacentHTML("beforeend", priceBtn.outerHTML);
+
+  // move head title on HYGIENE
+  hygieneHead.remove();
+  hygieneImg.insertAdjacentHTML("beforeend", hygieneHead.outerHTML);
+};
+
+if (scrWidth <= 768) {
+  moveEl();
+}
