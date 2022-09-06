ea534f1615c22ced39893d3c2bdd722e60fb7e25
colocolov <colocolov.stas@gmail.com>
2022-09-06 13:48:53 +0300

    add animate drops

---
+++ #src/js/scripts/animation.js
@@ -0,0 +1,23 @@
+// animation
+if (window.screen.width >= 768) {
+  function onEntry(entry) {
+    entry.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add("element-show");
+      } else {
+        change.target.classList.remove("element-show");
+      }
+    });
+  }
+
+  let options = {
+    threshold: [0.5],
+  };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll(".component__icon");
+
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+}
+// END
